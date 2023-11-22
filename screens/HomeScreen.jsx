85562ef32bd72b3b5e63778f36585f922ae1617c
marius.chitaru <marius.chitaru@gmail.com>
2023-11-22 15:21:23 +0200

    add Infinite scroll

--- screens/HomeScreen.jsx
+++ screens/HomeScreen.jsx
@@ -1,12 +1,48 @@
-import { FlatList } from 'react-native';
+import { ActivityIndicator, FlatList, RefreshControl } from 'react-native';
 import CoinItem from '../components/CoinItem';
-import data from '../assets/data/cryptocurrencies.json';
+import { useEffect, useState } from 'react';
+
+import { getMarketData } from "../lib/api";
 
 const HomeScreen = () => {
+
+  const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState(false);
+  const [data, setData] = useState([]);
+
+  const fetchMarketData = async () => {    
+
+    if(loading) return;
+    
+    setLoading(true);
+    const mdata = await getMarketData(page);
+    setData((prevData) => ([...prevData, ...mdata]));
+    setLoading(false);
+
+    console.log("fetch");
+  }
+
+  useEffect(() => {
+    fetchMarketData();
+  }, [page]);
+
+  if(loading || !data) {
+    return <ActivityIndicator size="large" />
+  }  
   return ( 
     <FlatList 
       data={data}
       renderItem={({item}) => (<CoinItem item={item}/>)}
+      keyExtractor={item => item.id}
+      onEndReachedThreshold={0.2}
+      onEndReached={() => setPage(data.length / 10 + 1)}
+      refreshControl={
+        <RefreshControl
+          refreshing={loading} 
+          tintColor="white"
+          onRefresh={() => setPage(1)}
+        />
+      }
     />
    );
 }
