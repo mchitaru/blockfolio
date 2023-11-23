755fb6e6f44eec3012bc85315b54df93310f5bbc
marius.chitaru <marius.chitaru@gmail.com>
2023-11-23 17:40:43 +0200

    bug fix

--- screens/AddAssetScreen.jsx
+++ screens/AddAssetScreen.jsx
@@ -95,7 +95,7 @@ const styles = StyleSheet.create({
   buttonText: {
     color: "white",
     fontSize: 17,
-    fontWight: "600"
+    fontWeight: "600"
   },
   pricePerCoin: {
     color: "grey",
